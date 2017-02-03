ec0b39cc5e6d08fe9216ed9a9caae628a584433f
EJ Schroeder <eman314@gmail.com>
2017-02-02 23:10:28 -0500

    Graph generation complete. Generates the correct number of states, and the children of the start state look correct.

--- src/Jewels.java
+++ src/Jewels.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * Created by EJ on 1/28/2017.
@@ -33,13 +32,28 @@ public class Jewels {
     private String startState;
     private String endState;
 
+    private static final int[][] jewelChanges = new int[][]{
+        {0,1,3},
+        {0,1,2,4},
+        {1,2,5},
+        {0,3,4,6},
+        {1,3,4,5,7},
+        {2,4,5,8},
+        {3,6,7},
+        {4,6,7,8},
+        {5,7,8},
+    };
+
     public Jewels(String startState, String goalState) {
+        stateEdgeMap = new HashMap<>();
+        this.startState = startState;
+        this.endState = goalState;
     }
 
     public static void main(String[] args){
         //System.out.println("Hello World!");
 
-        String startState = "";
+        String startState = "DDDDDDDDD";
         String goalState = "";
 
         Jewels j = new Jewels(startState, goalState);
@@ -47,6 +61,53 @@ public class Jewels {
         System.out.println( j.depthFirst() );
         System.out.println( j.bestFirst() );
 
+        j.generate();
+    }
+
+    public void generate() {
+        ArrayDeque<String> openQueue = new ArrayDeque<>();
+        openQueue.add(startState);
+
+        while (!openQueue.isEmpty()) {
+
+            String stateToEval = openQueue.remove();
+            if (!stateEdgeMap.containsKey(stateToEval)) {
+                String[] children = generateChildren(stateToEval);
+                Collections.addAll(openQueue, children);
+                stateEdgeMap.put(stateToEval, children);
+            }
+        }
+        System.out.println(stateEdgeMap.size());
+        System.out.println(Arrays.toString(stateEdgeMap.get(startState)));
+    }
+
+    private String[] generateChildren(String parentState) {
+        StringBuilder charSeq;
+        String[] children = new String[9];
+        for (int i = 0; i < 9; i++){
+            charSeq = new StringBuilder(parentState);
+
+            for (int j = 0; j < jewelChanges[i].length; j++){
+                char c = charSeq.charAt(jewelChanges[i][j]);
+                switch (c) {
+                    case 'D':
+                        c = 'R';
+                        break;
+                    case 'R':
+                        c = 'E';
+                        break;
+                    case 'E':
+                        c = 'D';
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Uhh... sumtin rong");
+                }
+                charSeq.setCharAt(jewelChanges[i][j], c);
+            }
+
+            children[i] = charSeq.toString();
+        }
+        return children;
     }
 
     private String bestFirst() {
