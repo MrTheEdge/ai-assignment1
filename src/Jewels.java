89724a3d6828d9134de8213ba87681cbddcd3245
EJ Schroeder <eman314@gmail.com>
2017-02-03 20:22:23 -0500

    Wrote comparator for use in the priority queue

--- src/Jewels.java
+++ src/Jewels.java
@@ -158,6 +158,14 @@ public class Jewels {
     }
 
 
+    private class HeuristicComp implements Comparator<State> {
+
+        @Override
+        public int compare(State o1, State o2) {
+            return o1.getWeight() - o2.getWeight();
+        }
+    }
+
     private class State {
 
         private String jewels;
